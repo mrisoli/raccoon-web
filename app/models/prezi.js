30522a9d2de6423c0349633248e1a7b0fc01e47d
mrisoli <celorisoli@gmail.com>
2016-06-28 20:16:05 -0300

    Adding other data to prezi component

--- app/models/prezi.js
+++ app/models/prezi.js
@@ -2,8 +2,8 @@ import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 
 export default Model.extend({
-  title: attr()
-  //thumbnail: attr(),
-  //creator: attr(),
-  //createdAt: attr()
+  title: attr(),
+  thumbnail: attr(),
+  creator: attr(),
+  createdAt: attr()
 });
